59ee47bcd4cd81a3a7002e2e49d17983e879114a
franciscols2011 <franciscols2011@gmail.com>
2022-05-06 23:33:51 -0300

    Se agrego el proyecto

---
+++ js/app.js
@@ -0,0 +1,324 @@
+var containerQuestionEl = document.getElementById("question-container");
+var containerStartEl = document.getElementById("starter-container");
+var containerEndEl = document.getElementById("end-container");
+var containerScoreEl = document.getElementById("score-banner");
+var formInitials = document.getElementById("initials-form");
+var containerHighScoresEl = document.getElementById("high-score-container");
+var ViewHighScoreEl = document.getElementById("view-high-scores");
+var listHighScoreEl = document.getElementById("high-score-list");
+var correctEl = document.getElementById("correct");
+var wrongEl = document.getElementById("wrong");
+var btnStartEl = document.querySelector("#start-game");
+var btnGoBackEl = document.querySelector("#go-back");
+var btnClearScoresEl = document.querySelector("#clear-high-scores");
+var questionEl = document.getElementById("question");
+var answerbuttonsEl = document.getElementById("answer-buttons");
+var timerEl = document.querySelector("#timer");
+var score = 0;
+var timeleft;
+var gameover;
+timerEl.innerText = 0;
+var HighScores = [];
+var arrayShuffledQuestions;
+var QuestionIndex = 0;
+
+var questions = [
+  {
+    q: "Los Arrays en Javascript se pueden utilizar para guardar __________.",
+    a: "4. todas son correctas",
+    choices: [
+      { choice: "1. numeros" },
+      { choice: "2. booleanos" },
+      { choice: "3. caracteres" },
+      { choice: "4. todas son correctas" },
+    ],
+  },
+  {
+    q: "Dentro de que etiqueta HTML ponemos nuestro Javascript?",
+    a: "3. <script>",
+    choices: [
+      { choice: "1. <h1>" },
+      { choice: "2. <js>" },
+      { choice: "3. <script>" },
+      { choice: "4. <head>" },
+    ],
+  },
+  {
+    q: "En el codigo -- setinterval(time(),1000) -- que hace time()?",
+    a: "1. llamar una funcion",
+    choices: [
+      { choice: "1. llamar una funcion" },
+      { choice: "2. undefined" },
+      { choice: "3. variable" },
+      { choice: "4. todas son correctas" },
+    ],
+  },
+  {
+    q: "Con que sintaxis llamamos una funcion?",
+    a: "4. function()",
+    choices: [
+      { choice: "1. var function" },
+      { choice: "2. function" },
+      { choice: "3. llamar funcion" },
+      { choice: "4. function()" },
+    ],
+  },
+  {
+    q: "Cuando aparecio JavaScript por primera vez?",
+    a: "1. 1995",
+    choices: [
+      { choice: "1. 1995" },
+      { choice: "2. 1990" },
+      { choice: "3. 2005" },
+      { choice: "4. 2000" },
+    ],
+  },
+  {
+    q: "Que significa DOM?",
+    a: "2. Document Object Model",
+    choices: [
+      { choice: "1. Do Overnight Modules" },
+      { choice: "2. Document Object Model" },
+      { choice: "3. Divas Obviously Model" },
+      { choice: "4. Do Oo Mo" },
+    ],
+  },
+  {
+    q: "Para que usualmente utilizamos el getItem?",
+    a: "2. almacenamiento local",
+    choices: [
+      { choice: "1. agregar un item" },
+      { choice: "2. almacenamiento local" },
+      { choice: "3. comprar online" },
+      { choice: "4. nombrar una variable" },
+    ],
+  },
+];
+
+var renderStartPage = function () {
+  containerHighScoresEl.classList.add("hide");
+  containerHighScoresEl.classList.remove("show");
+  containerStartEl.classList.remove("hide");
+  containerStartEl.classList.add("show");
+  containerScoreEl.removeChild(containerScoreEl.lastChild);
+  QuestionIndex = 0;
+  gameover = "";
+  timerEl.textContent = 0;
+  score = 0;
+
+  if ((correctEl.className = "show")) {
+    correctEl.classList.remove("show");
+    correctEl.classList.add("hide");
+  }
+  if ((wrongEl.className = "show")) {
+    wrongEl.classList.remove("show");
+    wrongEl.classList.add("hide");
+  }
+};
+
+var setTime = function () {
+  timeleft = 30;
+
+  var timercheck = setInterval(function () {
+    timerEl.innerText = timeleft;
+    timeleft--;
+
+    if (gameover) {
+      clearInterval(timercheck);
+    }
+
+    if (timeleft < 0) {
+      showScore();
+      timerEl.innerText = 0;
+      clearInterval(timercheck);
+    }
+  }, 1000);
+};
+
+var startGame = function () {
+  containerStartEl.classList.add("hide");
+  containerStartEl.classList.remove("show");
+  containerQuestionEl.classList.remove("hide");
+  containerQuestionEl.classList.add("show");
+  arrayShuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  setTime();
+  setQuestion();
+};
+var setQuestion = function () {
+  resetAnswers();
+  displayQuestion(arrayShuffledQuestions[QuestionIndex]);
+};
+
+var resetAnswers = function () {
+  while (answerbuttonsEl.firstChild) {
+    answerbuttonsEl.removeChild(answerbuttonsEl.firstChild);
+  }
+};
+
+var displayQuestion = function (index) {
+  questionEl.innerText = index.q;
+  for (var i = 0; i < index.choices.length; i++) {
+    var answerbutton = document.createElement("button");
+    answerbutton.innerText = index.choices[i].choice;
+    answerbutton.classList.add("btn");
+    answerbutton.classList.add("answerbtn");
+    answerbutton.addEventListener("click", answerCheck);
+    answerbuttonsEl.appendChild(answerbutton);
+  }
+};
+var answerCorrect = function () {
+  if ((correctEl.className = "hide")) {
+    correctEl.classList.remove("hide");
+    correctEl.classList.add("banner");
+    wrongEl.classList.remove("banner");
+    wrongEl.classList.add("hide");
+  }
+};
+
+var answerWrong = function () {
+  if ((wrongEl.className = "hide")) {
+    wrongEl.classList.remove("hide");
+    wrongEl.classList.add("banner");
+    correctEl.classList.remove("banner");
+    correctEl.classList.add("hide");
+  }
+};
+
+var answerCheck = function (event) {
+  var selectedanswer = event.target;
+  if (arrayShuffledQuestions[QuestionIndex].a === selectedanswer.innerText) {
+    answerCorrect();
+    score = score + 7;
+  } else {
+    answerWrong();
+    score = score - 1;
+    timeleft = timeleft - 3;
+  }
+
+  QuestionIndex++;
+  if (arrayShuffledQuestions.length > QuestionIndex + 1) {
+    setQuestion();
+  } else {
+    gameover = "true";
+    showScore();
+  }
+};
+
+var showScore = function () {
+  containerQuestionEl.classList.add("hide");
+  containerEndEl.classList.remove("hide");
+  containerEndEl.classList.add("show");
+
+  var scoreDisplay = document.createElement("p");
+  scoreDisplay.innerText = "Tu puntuacion final es " + score + "!";
+  containerScoreEl.appendChild(scoreDisplay);
+};
+
+var createHighScore = function (event) {
+  event.preventDefault();
+  var initials = document.querySelector("#initials").value;
+  if (!initials) {
+    alert("Ingresa tu nombre!");
+    return;
+  }
+
+  formInitials.reset();
+
+  var HighScore = {
+    initials: initials,
+    score: score,
+  };
+
+  HighScores.push(HighScore);
+  HighScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
+
+  for (var i = 0; i < HighScores.length; i++) {
+    var highscoreEl = document.createElement("li");
+    highscoreEl.ClassName = "high-score";
+    highscoreEl.innerHTML =
+      HighScores[i].initials + " = " + HighScores[i].score;
+    listHighScoreEl.appendChild(highscoreEl);
+  }
+
+  saveHighScore();
+  displayHighScores();
+};
+var saveHighScore = function () {
+  localStorage.setItem("HighScores", JSON.stringify(HighScores));
+};
+
+var loadHighScore = function () {
+  var LoadedHighScores = localStorage.getItem("HighScores");
+  if (!LoadedHighScores) {
+    return false;
+  }
+
+  LoadedHighScores = JSON.parse(LoadedHighScores);
+  LoadedHighScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+
+  for (var i = 0; i < LoadedHighScores.length; i++) {
+    var highscoreEl = document.createElement("li");
+    highscoreEl.ClassName = "high-score";
+    highscoreEl.innerText =
+      LoadedHighScores[i].initials + " = " + LoadedHighScores[i].score;
+    listHighScoreEl.appendChild(highscoreEl);
+
+    HighScores.push(LoadedHighScores[i]);
+  }
+};
+
+var displayHighScores = function () {
+  containerHighScoresEl.classList.remove("hide");
+  containerHighScoresEl.classList.add("show");
+  gameover = "true";
+
+  if ((containerEndEl.className = "show")) {
+    containerEndEl.classList.remove("show");
+    containerEndEl.classList.add("hide");
+  }
+  if ((containerStartEl.className = "show")) {
+    containerStartEl.classList.remove("show");
+    containerStartEl.classList.add("hide");
+  }
+
+  if ((containerQuestionEl.className = "show")) {
+    containerQuestionEl.classList.remove("show");
+    containerQuestionEl.classList.add("hide");
+  }
+
+  if ((correctEl.className = "show")) {
+    correctEl.classList.remove("show");
+    correctEl.classList.add("hide");
+  }
+
+  if ((wrongEl.className = "show")) {
+    wrongEl.classList.remove("show");
+    wrongEl.classList.add("hide");
+  }
+};
+
+var clearScores = function () {
+  HighScores = [];
+
+  while (listHighScoreEl.firstChild) {
+    listHighScoreEl.removeChild(listHighScoreEl.firstChild);
+  }
+
+  localStorage.clear(HighScores);
+};
+
+loadHighScore();
+
+btnStartEl.addEventListener("click", startGame);
+formInitials.addEventListener("submit", createHighScore);
+ViewHighScoreEl.addEventListener("click", displayHighScores);
+btnGoBackEl.addEventListener("click", renderStartPage);
+btnClearScoresEl.addEventListener("click", clearScores);
